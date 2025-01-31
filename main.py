b09cef9b6e14f244407a49d6d4378d4130d43ca3
Rudra <khandelwalrudra1305@gmail.com>
2025-01-31 21:20:57 +0530

    Fix import statement in main.py

--- main.py
+++ main.py
@@ -6,15 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware # type: ignore
 from routes import dashboard  
 import os
 
-
-app = FastAPI()
-app.add_middleware(
-       CORSMiddleware,
-       allow_origins=["*"],  
-       allow_credentials=True,
-       allow_methods=["*"],
-       allow_headers=["*"],
-   )
+app = FastAPI() 
 
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "Frontend")) 
 
